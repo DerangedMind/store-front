f33f06823d927ffe9832281760b673bc85251df3
Riki <1727164+DerangedMind@users.noreply.github.com>
2024-07-30 16:22:41 -0300

    add contact and product list example

--- src/home/home-page.ts
+++ src/home/home-page.ts
@@ -2,6 +2,7 @@ import { LitElement, html, css, TemplateResult, CSSResultArray, unsafeCSS } from
 import { customElement } from "lit/decorators.js";
 import splashImage from "../assets/stephanie-krist-zziuyr_T3jQ-unsplash.jpg";
 import pageCopy from "../i18n/en.json";
+import bouquetImg from "../assets/annie-spratt-uiWIMe_dMHU-unsplash.jpg";
 
 @customElement("home-page")
 export class HomePage extends LitElement {
@@ -10,23 +11,6 @@ export class HomePage extends LitElement {
             :host {
                 display: block;
             }
-
-            /* .hero {
-                min-height: 80vh;
-
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-                text-align: center;
-            }
-
-            .hero h1 {
-                font-size: 2rem;
-            }
-
-            .hero h2 {
-            } */
 
             .parallax {
                 perspective: 3px;
@@ -119,17 +103,87 @@ export class HomePage extends LitElement {
                 color: #111;
             }
 
-            .intro {
+            .intro,
+            .contact {
                 display: flex;
                 flex-direction: column;
                 align-items: center;
                 text-align: center;
+                padding: 0 4rem;
             }
 
-            section.intro p {
+            section.intro p,
+            section.contact p {
                 max-width: 50rem;
                 font-size: 1.25rem;
-                margin: inherit 2rem;
+            }
+
+            footer {
+                margin-top: 6rem;
+                padding: 1rem 0;
+                background-color: #111;
+                display: flex;
+                justify-content: center;
+                font-size: 0.875rem;
+            }
+
+            .store-preview ul {
+                display: flex;
+                gap: 2rem;
+                list-style: none;
+                margin: 0;
+                padding: 0;
+            }
+
+            .preview-item {
+                position: relative;
+            }
+
+            .preview-item .img-thumb {
+                font-size: 0;
+            }
+            .preview-item .img-thumb img {
+                width: 16rem;
+                height: auto;
+                object-fit: cover;
+                object-position: center right;
+            }
+
+            .preview-item .overlay {
+                position: absolute;
+                background-color: rgba(0, 0, 0, 0.5);
+                top: 0;
+                right: 0;
+                bottom: 0;
+                left: 0;
+            }
+            .preview-item .caption {
+                position: absolute;
+                bottom: 1rem;
+                right: 1rem;
+                z-index: 2;
+                color: white;
+            }
+
+            @media only screen and (max-width: 992px) {
+                .store-preview ul {
+                    flex-direction: column;
+                }
+
+                .preview-item {
+                    display: flex;
+                    justify-content: center;
+                    margin: 2rem;
+                }
+
+                .preview-item .img-thumb img {
+                    max-width: 30rem;
+                    width: 100%;
+                }
+
+                .preview-item .caption {
+                    font-size: 1.5rem;
+                }
             }
         `,
     ];
@@ -163,12 +217,43 @@ export class HomePage extends LitElement {
                     <hr />
                     <section class="store-preview">
                         <ul>
-                            <li>Item 1</li>
-                            <li>Item 2</li>
-                            <li>Item 3</li>
+                            <li class="preview-item">
+                                <a href="product/1">
+                                    <div class="img-thumb">
+                                        <img src=${bouquetImg} />
+                                    </div>
+                                    <div class="overlay"></div>
+                                    <div class="caption">Everyday bouquets</div>
+                                </a>
+                            </li>
+                            <li class="preview-item">
+                                <div class="img-thumb">
+                                    <img src=${bouquetImg} />
+                                </div>
+                                <div class="overlay"></div>
+                                <div class="caption">Everyday bouquets</div>
+                            </li>
+                            <li class="preview-item">
+                                <div class="img-thumb">
+                                    <img src=${bouquetImg} />
+                                </div>
+                                <div class="overlay"></div>
+                                <div class="caption">Everyday bouquets</div>
+                            </li>
                         </ul>
                     </section>
+
+                    <hr />
+
+                    <section class="contact">
+                        <h2>Contact us</h2>
+                        <p>${pageCopy.home.contact}</p>
+                        <a href="/contact">Click here to get in touch...</a>
+                    </section>
                 </main>
+                <footer>
+                    <i>Created by DerangedCreations</i>
+                </footer>
             </div>
         `;
     }
